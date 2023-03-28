c178b375c0c8bc5b93f80c7100c4090196d4256c
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-28 21:10:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,24 @@
 #include <map>
 #include "tstack.h"
 
+int getPrior(char ch) {
+    switch(ch) {
+        case"(":
+        return 0;
+        case")":
+        return 1;
+        case"+":
+        return 2;
+        case"-":
+        return 2;
+        case"*":
+        return 3;
+        case"/":
+        return 3;
+    }
+    return -1;
+}
+
 std::string infx2pstfx(std::string inf) {
   // добавьте код
   return std::string("");
