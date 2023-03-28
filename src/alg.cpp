ea3a13c484ab21badd65f945d18c1ee277906276
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-28 23:35:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,6 +5,22 @@
 
 TStack<char, 100> stack1;
 TStack<int, 100> stack2;
+    char ch0 = '0';
+    char ch1 = '1';
+    char ch2 = '2';
+    char ch3 = '3';
+    char ch4 = '4';
+    char ch5 = '5';
+    char ch6 = '6';
+    char ch7 = '7';
+    char ch8 = '8';
+    char ch9 = '9';
+    char chM = '-';
+    char chP = '+';
+    char chD = '/';
+    char chMlt = '*';
+    char ch9Rsk = ')';
+    char ch9Lsk = '(';
 
 int getPrior(char ch) {
     switch(ch) {
@@ -25,31 +41,28 @@ int getPrior(char ch) {
 }
 
 std::string infx2pstfx(std::string inf) {
-    std::string out = "";
-    char chars[inf.length() + 1];
-    inf.copy(chars, inf.length() + 1);
+   std::string out = "";
     for (int i = 0; i < inf.length(); i++) {
-        if (chars[i] == "0" || chars[i] == "1"|| chars[i] == "2"|| chars[i] == "3"|| chars[i] == "4" || chars[i] == "5"|| chars[i] == "6"|| chars[i] == "7"|| chars[i] == "8"|| chars[i] == "9") {
-            out += " " + chars[i];
+        if (inf.at(i) == ch0 || inf.at(i) == ch1 || inf.at(i) == ch2 || inf.at(i) == ch3 || inf.at(i) == ch4 || inf.at(i) == ch5 || inf.at(i) == ch6 || inf.at(i) == ch7 || inf.at(i) == ch8 || inf.at(i) == ch9) {
+            out += " " + inf.at(i);
         }
-        if (chars[i] == "(" || chars[i] == ")" || chars[i] == "+" || chars[i] == "-" || chars[i] == "*" || chars[i] == "/" && getPrior(chars[i]) > getPrior(stack1.get()) || stack1.isEmpty()) {
-            stack1.push(chars[i]);
+        if ((inf.at(i) == ch9Lsk || inf.at(i) == ch9Rsk || inf.at(i) == chP || inf.at(i) == chM || inf.at(i) == chMlt || inf.at(i) == chD || stack1.isEmpty()) && (getPrior(inf.at(i)) > getPrior(stack1.get()))) {
+            stack1.push(inf.at(i));
         }
-        if (chars[i] == "(" || chars[i] == ")" || chars[i] == "+" || chars[i] == "-" || chars[i] == "*" || chars[i] == "/" && getPrior(chars[i]) <= getPrior(stack1.get())) {
+        if ((inf.at(i) == ch9Lsk || inf.at(i) == ch9Rsk || inf.at(i) == chP || inf.at(i) == chM || inf.at(i) == chMlt || inf.at(i) == chD) && (getPrior(inf.at(i)) <= getPrior(stack1.get()))) {
             out += " " + stack1.pop();
-            if (stack1.get() == "(") {
+            if (stack1.get() == ch9Lsk) {
                 stack1.pop();
             }
         }
     }
     while (stack1.isEmpty() == false) {
         out += " " + stack1.pop();
-        if (stack1.get() == "(") {
+        if (stack1.get() == ch9Lsk) {
             stack1.pop();
         }
     }
     return out;
-  return std::string("");
 }
 
 int eval(std::string pref) {
