637ca6e2706fb14ed838d8a986010e408924e3e3
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-28 21:47:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,6 +3,9 @@
 #include <map>
 #include "tstack.h"
 
+TStack<char, 100> stack1;
+TStack<int, 100> stack2;
+
 int getPrior(char ch) {
     switch(ch) {
         case"(":
@@ -22,7 +25,32 @@ int getPrior(char ch) {
 }
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
+    std::string out = "";
+    // копируем символы строки inf в массив chars
+    char chars[inf.length + 1];
+    inf.copy(chars, inf.length + 1);
+    for (int i = 0; i < chars.length; i++) {
+        if (chars[i] == "0" || chars[i] == "1"|| chars[i] == "2"|| chars[i] == "3"|| chars[i] == "4" || chars[i] == "5"|| chars[i] == "6"|| chars[i] == "7"|| chars[i] == "8"|| chars[i] == "9") {
+            out += chars[i];
+            //stack1.push(chars[i]);
+        }
+        if (chars[i] == "(" || chars[i] == ")" || chars[i] == "+" || chars[i] == "-" || chars[i] == "*" || chars[i] == "/" && getPrior(chars[i]) > getPrior(stack1.get()) || stack1.isEmpty()) {
+            stack1.push(chars[i]);
+        }
+        if (chars[i] == "(" || chars[i] == ")" || chars[i] == "+" || chars[i] == "-" || chars[i] == "*" || chars[i] == "/" && getPrior(chars[i]) <= getPrior(stack1.get())) {
+            out += stack1.pop();
+            if (stack1.get() == "(") {
+                stack1.pop();
+            }
+        }
+    }
+    while (stack1.isEmpty() == false) {
+        out += stack1.pop();
+        if (stack1.get() == "(") {
+            stack1.pop();
+        }
+    }
+    return out;
   return std::string("");
 }
 
