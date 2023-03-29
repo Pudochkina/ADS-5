eeeeb8032bff5fa92f5d97c0672a667f68c72c88
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-29 23:35:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -22,76 +22,105 @@ int getPrior(char ch) {
     }
     return -1;
 }
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
-    std::string post;
-    TStack<char, 100> stck;
-    for (int i = 0; i < inf.size(); i++) {
-        if ((inf[i] <= '9') && (inf[i] >= '0')) {
-            post += inf[i];
-            post += " ";
-        } else {
-            int k1 = getPrior(inf[i]);
-            int k2 = getPrior(stck.get());
-            if (inf[i] == '(' || (k1 > k2) || stck.isEmpty()) {
-                stck.push(inf[i]);
-            } else if (inf[i] == ')') {
-                char s = stck.pop();
-                while (s != '(') {
-                    post += s;
-                    post += " ";
-                    s = stck.pop();
+std::string infx2pstfx(std::string inf) { 
+   TStack<char, 100> stack1;
+   std::string out = "";
+    for (int i = 0; i < inf.length(); i++) {
+        if (inf[i] == ch0 || inf[i] == ch1 || inf[i] == ch2 || inf[i] == ch3
+            || inf[i] == ch4 || inf[i] == ch5 || inf[i] == ch6 || inf[i] == ch7 
+|| inf[i] == ch8 || inf[i] == ch9) {
+            out += inf[i] + " ";
+        }
+        else if (inf[i] == ch9Lsk || stack1.isEmpty() 
+|| getPrior(inf[i]) > getPrior(stack1.get())) {
+            stack1.push(inf[i]);
+        } else if (getPrior(inf[i] <= stack1.get())) {
+                while (getPrior(inf[i] <= stack1.get()) && !stack1.isEmpty()) {
+                    out +=  stack1.pop() + " ";
                 }
-            } else if (getPrior(inf[i] <= stck.get())) {
-                while (getPrior(inf[i] <= stck.get()) && !stck.isEmpty()) {
-                    char s = stck.pop();
-                    post += s;
-                    post += " ";
-                }
-                stck.push(inf[i]);
+                stack1.push(inf[i]);
             }
+        /*else if (getPrior(inf[i]) <= getPrior(stack1.get())) {
+            out += stack1.get() + " ";
+            stack1.pop();
+            if (stack1.get() == ch9Lsk) {
+                stack1.pop();
+            }
+            if (inf[i]!=ch9Lsk) {
+                stack1.push(inf[i]);
+            }
+        }*/
+        
+         else if (inf[i] == ch9Rsk) {
+                char ch = stack1.pop();
+                while (ch != ch9Lsk) {
+                    out += ch + " ";
+                    ch = stack1.pop();
+                }
+            } 
+    }
+    while (!stack1.isEmpty()) {
+        if (stack1.get() == ch9Lsk) {
+            stack1.pop();
+        } else {
+        out += stack1.get() + " ";
+        stack1.pop();
         }
     }
-    while (!stck.isEmpty()) {
-        char s = stck.pop();
-        post += s;
-        post += " ";
-    }
-    post.pop_back();
-    return post;
+    return out;
 }
 
 int eval(std::string pref) {
-    TStack <int, 100> temp;
-    for (char s : pref) {
-        if (s == ' ')
-            continue;
-        if ((s <= '9') && (s >= '0')) {
-            int z = static_cast<int>(s - '0');
-            temp.push(z);
-        } else {
-            int zn;
-            int first = temp.get();
-            temp.pop();
-            int second = temp.get();
-            temp.pop();
-            switch (s) {
-            case '+':
-                zn = second + first;
-                break;
-            case '-':
-                zn = second - first;
-                break;
-            case '*':
-                zn = second * first;
-                break;
-            case '/':
-                zn = second / first;
-                break;
-            }
-            temp.push(zn);
-        }
-    }
-    return temp.pop();
+  TStack<int, 100> stack2;
+  int result;
+  char p = ' ';
+  for (int i = 0; i < pref.length(); i++) {
+      if (pref[i] == ch0 || pref[i] == ch1 || pref[i] == ch2 || pref[i] == ch3 
+|| pref[i] == ch4
+          || pref[i] == ch5 || pref[i] == ch6 || pref[i] == ch7 || pref[i] == ch8 
+|| pref[i] == ch9) {
+          int arg = 0;
+          char ch = pref[i];
+          arg = static_cast<int>(ch - '0');
+          stack2.push(arg);
+      }
+      if (pref[i] == p) {
+      continue;
+      } 
+      if (pref[i] == chM) {
+          int arg1 = stack2.get();
+          stack2.pop();
+          int arg2 = stack2.get();
+          stack2.pop();
+          int res = arg1-arg2;
+          stack2.push(res);
+      }
+      if (pref[i] == chP) {
+          int arg1 = stack2.get();
+          stack2.pop();
+          int arg2 = stack2.get();
+          stack2.pop();
+          int res = arg1+arg2;
+          stack2.push(res);
+      }
+      if (pref[i] == chMlt) {
+          int arg1 = stack2.get();
+          stack2.pop();
+          int arg2 = stack2.get();
+          stack2.pop();
+          int res = arg1*arg2;
+          stack2.push(res);
+      }
+      if (pref[i] == chD) {
+          int arg1 = stack2.get();
+          stack2.pop();
+          int arg2 = stack2.get();
+          stack2.pop();
+          int res = arg1/arg2;
+          stack2.push(res);
+      }
+  }
+  result = stack2.get();
+  stack2.pop();
+  return result;
 }
