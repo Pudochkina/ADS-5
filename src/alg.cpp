c162deb1a832d22a78571963ef671bb9fa38181d
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-29 21:19:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -63,15 +63,15 @@ int eval(std::string pref) {
   TStack<int, 100> stack2;
   int result;
   for (int i = 0; i < pref.length(); i++) {
-      if (pref[i] == ' ') {
-      continue;
-      } 
       if (pref[i] == ch0 || pref[i] == ch1 || pref[i] == ch2 || pref[i] == ch3 || pref[i] == ch4 || pref[i] == ch5 || pref[i] == ch6 || pref[i] == ch7 || pref[i] == ch8 || pref[i] == ch9) {
           int arg = 0;
           char ch = pref[i];
           arg = ch - '0';
           stack2.push(arg);
       }
+      if (pref[i] == ' ') {
+      continue;
+      } 
       if (pref[i] == chM) {
           int arg1 = stack2.get();
           stack2.pop();
