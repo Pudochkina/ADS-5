2892190cc2305ae9404fbae032d467649f09a871
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-03-29 21:25:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include "tstack.h"
 
-TStack<int, 100> stack2;
 char ch0 = '0', ch1 = '1', ch2 = '2', ch3 = '3', ch4 = '4', ch5 = '5',
 ch6 = '6', ch7 = '7', ch8 = '8', ch9 = '9', chM = '-',
 chP = '+', chD = '/', chMlt = '*', ch9Rsk = ')', ch9Lsk = '(';
@@ -28,23 +27,26 @@ std::string infx2pstfx(std::string inf) {
    TStack<char, 100> stack1;
    std::string out = "";
     for (int i = 0; i < inf.length(); i++) {
-        if (inf.at(i) == ch0 || inf.at(i) == ch1 || inf.at(i) == ch2 || inf.at(i) == ch3 || inf.at(i) == ch4 || inf.at(i) == ch5 || inf.at(i) == ch6 || inf.at(i) == ch7 || inf.at(i) == ch8 || inf.at(i) == ch9) {
-            out += inf.at(i) + " ";
+        if (inf[i] == ch0 || inf[i] == ch1 || inf[i] == ch2 || inf[i] == ch3
+            || inf[i] == ch4 || inf[i] == ch5 || inf[i] == ch6 || inf[i] == ch7 || inf[i] == ch8 || inf[i] == ch9) {
+            out += inf[i] + " ";
         }
-        if (inf.at(i) == ch9Lsk || stack1.isEmpty() && inf.at(i) == chP || inf.at(i) == chM || inf.at(i) == chMlt || inf.at(i) == chD) {
-            stack1.push(inf.at(i));
+        if (inf[i] == ch9Lsk || stack1.isEmpty() && inf[i] == chP || inf[i] == chM || inf[i] == chMlt || inf[i] == chD) {
+            stack1.push(inf[i]);
         }
-        if ((inf.at(i) == ch9Lsk || inf.at(i) == ch9Rsk || inf.at(i) == chP || inf.at(i) == chM || inf.at(i) == chMlt || inf.at(i) == chD  && getPrior(inf.at(i)) > getPrior(stack1.get())) || stack1.isEmpty()) {
-            stack1.push(inf.at(i));
+        if ((inf[i] == ch9Lsk || inf[i] == ch9Rsk || inf[i] == chP || inf[i] == chM || inf[i] == chMlt
+             || inf[i] == chD  && getPrior(inf[i]) > getPrior(stack1.get())) || stack1.isEmpty()) {
+            stack1.push(inf[i]);
         }
-        if ((inf.at(i) == ch9Lsk || inf.at(i) == ch9Rsk || inf.at(i) == chP || inf.at(i) == chM || inf.at(i) == chMlt || inf.at(i) == chD) && (getPrior(inf.at(i)) <= getPrior(stack1.get()))) {
+        if ((inf[i] == ch9Lsk || inf[i] == ch9Rsk || inf[i] == chP || inf[i] == chM
+             || inf[i] == chMlt || inf[i] == chD) && (getPrior(inf[i]) <= getPrior(stack1.get()))) {
             out += stack1.get() + " ";
             stack1.pop();
             if (stack1.get() == ch9Lsk) {
                 stack1.pop();
             }
-            if (inf.at(i)!=ch9Lsk) {
-                stack1.push(inf.at(i));
+            if (inf[i]!=ch9Lsk) {
+                stack1.push(inf[i]);
             }
         }
     }
@@ -63,7 +65,8 @@ int eval(std::string pref) {
   TStack<int, 100> stack2;
   int result;
   for (int i = 0; i < pref.length(); i++) {
-      if (pref[i] == ch0 || pref[i] == ch1 || pref[i] == ch2 || pref[i] == ch3 || pref[i] == ch4 || pref[i] == ch5 || pref[i] == ch6 || pref[i] == ch7 || pref[i] == ch8 || pref[i] == ch9) {
+      if (pref[i] == ch0 || pref[i] == ch1 || pref[i] == ch2 || pref[i] == ch3 || pref[i] == ch4
+          || pref[i] == ch5 || pref[i] == ch6 || pref[i] == ch7 || pref[i] == ch8 || pref[i] == ch9) {
           int arg = 0;
           char ch = pref[i];
           arg = ch - '0';
